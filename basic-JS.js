8bbea1ab46093f002cc6c29036e1bb8b2646e9f7
Maciej Cupial <mj.cupial@gmail.com>
2018-11-02 19:46:30 +0100

    smooth scrolling and animations

--- basic-JS.js
+++ basic-JS.js
@@ -2,18 +2,28 @@
 // ========================================
 //      BASIC SCRIPT FOR INDEX.HTML
 // ========================================
-
-
-$('#sweetalert').on('click', function() {
-  swal({
-    type: 'error',
-    title: 'Sorry',
-    text: 'but this section is not available yet',
-  });
-});
-
+//
 
 $(document).ready(function() {
+  // ======================================================
+  //      short animations for specific elements
+  // ======================================================
+  $(".profile-img").addClass("animated fadeInUp");
+
+
+  // ======================================================
+  // when something is clicked but the content is not ready
+  // ======================================================
+  $('.sth').on('click', function() {
+    swal({
+      type: 'error',
+      title: 'Sorry',
+      text: 'but this section is not available yet',
+    });
+  });
+  // ============================================
+  //      button btn-status-1 is clicked
+  // ============================================
   $("#btn-status-1").click(function() {
     var elem = $("#btn-status-1").text();
     if (elem == "Read more") {
@@ -22,6 +32,9 @@ $(document).ready(function() {
       $("#btn-status-1").text("Read more");
     }
   });
+  // ============================================
+  //      button btn-status-2 is clicked
+  // ============================================
   $("#btn-status-2").click(function() {
     var elem = $("#btn-status-2").text();
     if (elem == "Read more") {
@@ -30,4 +43,15 @@ $(document).ready(function() {
       $("#btn-status-2").text("Read more");
     }
   });
+
+  $('html, body').animate({
+      scrollTop: $("#education").offset().top
+  }, 1000);
+
+
+
+
+
+
+  //end of document ready function
 });
