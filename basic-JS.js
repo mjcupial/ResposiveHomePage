faa4c042191be66366dc4c88c75dc577538c0dfb
Maciej Cupial <mj.cupial@gmail.com>
2018-11-02 17:34:34 +0100

    read more / read less

--- basic-JS.js
+++ basic-JS.js
@@ -1,6 +1,33 @@
-$(document).ready(function(){
-  $('#sweetalert').on('click', function() {
-    swal("Sorry,", "but this section is not available yet", "error");
-  });
+//
+// ========================================
+//      BASIC SCRIPT FOR INDEX.HTML
+// ========================================
 
-};
+
+$('#sweetalert').on('click', function() {
+  swal({
+    type: 'error',
+    title: 'Sorry',
+    text: 'but this section is not available yet',
+  });
+});
+
+
+$(document).ready(function() {
+  $("#btn-status-1").click(function() {
+    var elem = $("#btn-status-1").text();
+    if (elem == "Read more") {
+      $("#btn-status-1").text("Read less");
+    } else {
+      $("#btn-status-1").text("Read more");
+    }
+  });
+  $("#btn-status-2").click(function() {
+    var elem = $("#btn-status-2").text();
+    if (elem == "Read more") {
+      $("#btn-status-2").text("Read less");
+    } else {
+      $("#btn-status-2").text("Read more");
+    }
+  });
+});
